8eaf757160ea3a7fd7521208019c099e906b024b
Francisco Freitas Jr <fresfj@gmail.com>
2023-02-14 11:23:14 -0300

    add

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "jdbc:postgresql://strapi.c67dl1nneneb.us-east-1.rds.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi123"),
+    },
+    useNullAsDefault: true,
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
