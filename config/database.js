73ae3cd08409741adb5c6c6830e69219b7770b2b
Francisco Freitas Jr <fresfj@gmail.com>
2023-02-23 15:20:49 -0300

    Implement Internationalization

--- config/database.js
+++ config/database.js
@@ -15,13 +15,3 @@ module.exports = ({ env }) => ({
     debug: false,
   },
 });
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//     },
-//     useNullAsDefault: true,
-//     debug: false,
-//   },
-// });
